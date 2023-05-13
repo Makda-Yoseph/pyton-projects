578a799aa3c93477bf96fe051ee8e11d828ff028
Makda-Yoseph <124851414+Makda-Yoseph@users.noreply.github.com>
2023-05-14 02:54:26 +0300

    putting the right guessed letter in place

--- hangman_game.py
+++ hangman_game.py
@@ -2,8 +2,12 @@ import random
 word_list=["aardvark","baboon","camel"]
 chosen_word=random.choice(word_list)
 guess= input(str("guess a leter.")).lower()
+display=[]
+n=0
 for i in chosen_word:
     if guess== i:
-        print("Right")
+        display.insert(n,i)
+        n +=1
     else:
-        print("Wrong")
+        display.insert(n,"_")
+        n +=1
